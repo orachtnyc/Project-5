5fe7484af7857132ad172a5b91d2dd21c447447e
CaroCynth <orachtnyc@gmail.com>
2021-10-20 22:46:42 +0800

    fix code

--- src/serviceWorker.js
+++ src/serviceWorker.js
@@ -30,7 +30,7 @@ function registerValidSW(swUrl, config) {
           return;
         }
         installingWorker.onstatechange = () => {
-          if (installingWorker.state === 'installed') 
+          if (installingWorker.state === 'installed') {
             if (navigator.serviceWorker.controller) {
               // At this point, the updated precached content has been fetched,
               // but the previous service worker will still serve the older
